d7b5cfa8516cc84beaaf740cea06fb85e158cfbe
DonnieNgK <donniengk@gmail.com>
2023-05-29 17:14:05 -0400

    Added TODO for new functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,14 @@ class TodoApp extends StatelessWidget {
   }
 
 }
+
+
+/*
+TODO
+-Show a message saying "No todo exists. Please create one and track your work", if no todo was created.
+-BUG: Todos with same name if deleted both are deleted. 
+  -tip for fix > Assign a random id for each todo and while deleting, filter the todo by id.
+-Add functionality to edit the name of a todo
+Add a functionality to store the todo in the device storage. Show the todos to the user when they reopen the app by reading them from your device storage.
+
+*/
